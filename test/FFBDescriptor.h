4fed3c7a1c117e654105ac80291c960adb0e15e4
Arden Powl <ardenpowl@gmail.com>
2025-04-11 00:31:27 -0700

    aa

---
+++ test/FFBDescriptor.h
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef _FFBH
+#define _FFBH
+
+static const uint8_t _reportDescriptor[] PROGMEM = {
+  0x05, 0x0f
+}
+
+#endif // #ifndef _FFBH
--- test/test.ino
+++ test/test.ino
@@ -1,9 +1,38 @@
 #include <Arduino.h>
-#include <HID.h>
+#include <PluggableUSB.h>
 
-//#include "logger.h"
+#include "HIDDescriptor.h"
 
 int main(){
   for(;;);
 }
+
+
+/*
+const int BUTTON_PIN = 2; // Declare the button pin
+const int X_PIN = A0;     // Declare the X-axis pin
+const int Y_PIN = A1;     // Declare the Y-axis pin
+
+void setup() {
+  static HIDSubDescriptor node(_hidReportDescriptor, sizeof(_hidReportDescriptor));
+  HID().AppendDescriptor(&node);
+}
+
+struct JoystickReport {
+  uint8_t buttons; // 4 buttons (bits 0-3)
+  uint8_t x;       // X-axis (0-255)
+  uint8_t y;       // Y-axis (0-255)
+};
+
+void loop() {
+  JoystickReport report = {
+    .buttons = (digitalRead(BUTTON_PIN) == LOW) ? 0b0001 : 0b0000, // Read the button state
+    .x = analogRead(X_PIN) >> 2, // Convert 10-bit ADC to 8-bit
+    .y = analogRead(Y_PIN) >> 2
+  };
+
+  HID().SendReport(1, &report, sizeof(report));
+  delay(10);
+}
+*/
 
